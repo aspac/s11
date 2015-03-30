08121326f76528ba01d3dfc53fdfd5981578899a
dana satriya <dana_satriya@yahoo.com>
2015-03-30 21:44:35 +0200

    touch stack, no more header

--- s11py/stack/stack.py
+++ s11py/stack/stack.py
@@ -181,9 +181,8 @@ class BEARER_CONT_4(Structure):
     _pack_ = 1
     _fields_ = [("m_type", c_ubyte),("m_length", c_ushort),
                 ("flags", c_ubyte), ("eb_id", c_ubyte), 
-                 ("ie_charging", CHARG_CHAR)
-                ("ftid", FTEID), ("ft_id", FTEID), 
-                ("tft1",TAD)]
+                 ("ie_charging", CHARG_CHAR),
+                ("ftid", FTEID), ("ft_id", FTEID), ("tft1",TAD)]
 
 class BEARER_CONT_5(Structure):
     _pack_ = 1
@@ -219,33 +218,3 @@ class NODETYPE(Structure):
                 ("flags", c_ubyte), ("value", c_ubyte)]    
                 
 #--------------------------- Bootstrap --------------------------------------
-if __name__ == '__main__':
-     m = GTP()
-     if m:
-         m.flags=0x48
-         m.m_type=0x20
-         m.teid=0x000000
-         m.spare=0x000000
-     else:
-         exit(1)
-         
-     print m.m_type
-     print m.flags
-
-     x = MSISDN()
-     print x.msisdn_id
-
-     pf = PACKET_FILTER(1,2,3,4,5)
-     print pf.packet_id
-     
-     print "check TAD.."
-     tad = TAD(1,2,3,4,5,6,pf)
-     print tad.p_filter.packet_id, tad.p_filter.port_type
-    # print tad.p_filter.packet_id
-     
-     
-     UDP_IP = "127.0.0.1"
-     sock = socket.socket(socket.AF_INET, # Internet
-                        socket.SOCK_DGRAM) #
-                        
-     sock.sendto(m, ("127.0.0.1", _GTP_PORT))
