050e4ae62e98ed83efa7369aa65eb8605836b700
dana satriya <dana_satriya@yahoo.com>
2015-04-01 23:39:36 +0200

    tread water, at least shows the IEs on out-wire..

--- s11py/app/mme.py
+++ s11py/app/mme.py
@@ -6,6 +6,8 @@ Created on Mon Mar 30 21:35:11 2015
 """
 
 import sys,os.path, socket
+
+
 #add other libraries in the sys.path
 if __name__ == '__main__': 
     f = os.path.dirname(sys.path.pop(0))
@@ -13,7 +15,14 @@ if __name__ == '__main__':
 
 from stack.ie import *
 from stack.preproc import *
+import binascii
 
+def msglen_htons(msg):
+  return socket.htons(msg)
+
+def bin_encoder(msisdn):
+     for field_name, field_type in msisdn._fields_:
+         print field_name + "is" , field_type
 
 if __name__ == '__main__':
         
@@ -35,16 +44,54 @@ if __name__ == '__main__':
      print x.msisdn_id
 
      pf = PACKET_FILTER(1,2,3,4,5)
-     print pf.packet_id
-     
+         
      print "check TAD.."
      tad = TAD(1,2,3,4,5,6,pf)
-     print tad.p_filter.packet_id, tad.p_filter.port_type
+     
     # print tad.p_filter.packet_id
+     imsi = IMSI()
+     imsi.m_type=0x01;
+     #TO DO : m_length class 
+     imsi.m_length=socket.htons(8)
+     imsi.flags=0x00;
+     imsi.ims_id= 0x1000000004410622;
      
+    
+     msisdn = MSISDN()
+     msisdn.m_type = 0x4c;
+     msisdn.m_length = msglen_htons(6); #4 byte, if not append 00 0something
+     msisdn.flags=0x00;
+     #msisdn.msisdn_id=0x021012307094;  
+     msisdn.msisdn_id=0x100000007040; 
+
+    #ToDo : check on hex
+     #1, check C_types type and check len , of not append with 0x0
+     print "value is ", hex(msisdn.flags)
      
+                     
+     gtp = GTP()
+     gtp.flags = 0x48
+     gtp.m_type = 0x20
+     #gtp.m_length  = 8 + sizeof(imsi)
+     gtp.m_length = socket.htons(8 + sizeof(imsi)+sizeof(msisdn))
+     #gtp.m_length = 
+     gtp.teid = socket.htonl(0x000000);
+     gtp.seq_no=socket.htons(0x0001);
+     gtp.spare=0x0000;
+                
+    
+     #..to do  : wrapper   
+     imsi_buf = string_at(byref(imsi), sizeof(imsi))         
+     msisdn_buf = string_at(byref(msisdn), 10)     
+     gtp_buf = string_at(byref(gtp), sizeof(gtp))
+     
+     #print "size of gtp_buf is ", sizeof(gtp)
+     #print "MSISDN buff is ", binascii.hexlify(msisdn_buf)
+     buf = gtp_buf + imsi_buf + msisdn_buf
+                        
      UDP_IP = "127.0.0.1"
      sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) #
                         
-     sock.sendto(m, ("127.0.0.1", stdVar.GTP_PORT))
+     sock.sendto(buf, (UDP_IP, stdVar.GTP_PORT))
+                        
