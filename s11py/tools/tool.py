fd9370eb5e890ce6d409e04c0cda6717f7ac30e3
dana satriya <dana_satriya@yahoo.com>
2015-04-14 21:42:54 +0200

    add hex tool for bytestream conversion

---
+++ s11py/tools/tool.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 21:35:11 2015
+
+@author: dana
+"""
+
+   
+class HEX_TOOL(object):
+    
+      
+    def hex_convert_from_int(self, hexstring, base):
+         '''Convert a hextring with a specified base to integer'''
+         return int(hex(hexstring), base)
+       
